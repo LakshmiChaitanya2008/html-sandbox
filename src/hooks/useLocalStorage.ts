67579dc4b13fce39740c1e9dc6f4ad563d78f4f8
LakshmiChaitanya2008 <lakshmichaitanyachalamalasetti@gmail.com>
2021-09-28 19:15:24 +0530

    chore: html update

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -2,7 +2,25 @@ import { useState } from "react";
 
 const useLocalStorage = (key: string) => {
   const [value, setValue] = useState(() => {
-    const item = localStorage.getItem(key);
+    let item;
+
+    if (key === "html") {
+      item =
+        localStorage.getItem("html") ||
+        `<h1>HTML SANDBOX!</h1>
+<img src="https://c.tenor.com/u9XnPveDa9AAAAAM/rick-rickroll.gif" />
+      `;
+    } else if (key === "css") {
+      item =
+        localStorage.getItem("css") ||
+        `body{
+  background-color: #272b33;
+  text-align:center;
+  color: #eee;
+  font-family: "Arial", sans-serif;
+}
+`;
+    } else item = localStorage.getItem("js");
 
     return item ? item : "";
   });
